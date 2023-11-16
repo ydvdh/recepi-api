1904c1ecaecca368b40dc23f6fcd44c8d4e626d3
ydvdh <yadavdhkl@gmail.com>
2023-11-16 08:19:50 -0500

    Create user and super user

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Recepi API using Django
 1. Docker file
 2. Docker ignore
-    
+
     Run \recepi-api>docker build .
 
 3. Add docker-compose.yml file --> recepi-api>docker-compose build
@@ -12,3 +12,7 @@
 7. Running the test project:\recepi-api>docker-compose run --rm app sh -c "python manage.py test"
 8. Creating new core project: docker-compose run --rm app sh -c "python manage.py startapp core"
 9. Checking linting: docker-compose run --rm app sh -c "python manage.py wait_for_db && flake8"
+10. Migration: docker-compose run --rm app sh -c "python manage.py makemigrations"
+               docker-compose run --rm app sh -c "python manage.py wait_for_db && python manage.py migrate"
+
+11. Creating super user: docker-compose run --rm app sh -c "python manage.py createsuperuser"
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -10,25 +10,23 @@ from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-@patch('core.management.commands.wait_for_db.Command.check')
+@patch('django.db.utils.ConnectionHandler.__getitem__')
 class CommandTests(SimpleTestCase):
     """Test commands."""
 
-    def test_wait_for_db_ready(self, patched_check):
+    def test_wait_for_db_ready(self, patched_getitem):
         """Test waiting for database if database ready."""
-        patched_check.return_value = True
+        patched_getitem.return_value = True
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(databases=['default'])
+        self.assertEqual(patched_getitem.call_count, 1)
 
     @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+    def test_wait_for_db_delay(self, patched_sleep, patched_getitem):
         """Test waiting for database when getting OperationalError."""
-        patched_check.side_effect = [Psycopg2OpError] * 2 + \
-            [OperationalError] * 3 + [True]
+        patched_getitem.side_effect = [Psycopg2OpError] * 2 + [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
-        self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(databases=['default'])
+        self.assertEqual(patched_getitem.call_count, 6)
